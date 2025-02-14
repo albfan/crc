a2bd0e1015bd784a58547aceb723ee885221476c
Alberto Fanjul <afanjula@redhat.com>
2025-02-14 13:32:15 +0100

    fix cmd test

--- test/extended/crc/cmd/cmd_test.go
+++ test/extended/crc/cmd/cmd_test.go
@@ -9,6 +9,7 @@ import (
 func TestCommand(t *testing.T) {
 	assert.Equal(t, "crc setup", Command{
 		command: "setup",
+		updateCheck: true,
 	}.ToString())
 	assert.Equal(t, "CRC_DEBUG_ENABLE_STOP_NTP=true crc start -b bundle", Command{
 		command:     "start -b bundle",
@@ -18,5 +19,6 @@ func TestCommand(t *testing.T) {
 	assert.Equal(t, "CRC_DISABLE_UPDATE_CHECK=true CRC_DEBUG_ENABLE_STOP_NTP=true crc start -b bundle", Command{
 		command:    "start -b bundle",
 		disableNTP: true,
+		updateCheck: false,
 	}.ToString())
 }
